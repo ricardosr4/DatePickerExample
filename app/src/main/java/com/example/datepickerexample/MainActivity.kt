89e013bb16a0b90887710258d3dc176d9e1c7301
Ricardo <ricardosr4@gmail.com>
2023-01-25 10:00:36 -0300

    avance datepicker

--- app/src/main/java/com/example/datepickerexample/MainActivity.kt
+++ app/src/main/java/com/example/datepickerexample/MainActivity.kt
@@ -8,22 +8,24 @@ import java.time.Month
 import java.time.Year
 
 class MainActivity : AppCompatActivity() {
+    var etDate: EditText? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val etDate = findViewById<EditText>(R.id.etDate)
-        etDate.setOnClickListener { showDataPickerDialog() }
+         etDate = findViewById<EditText>(R.id.etDate)
+        etDate!!.setOnClickListener { showDataPickerDialog() }
 
 
     }
 
     private fun showDataPickerDialog() {
         val datePicker = DatePickerFragment {day, month, year -> onDateSelected(day, month, year)}
-        datePicker.show(supportFragmentManager, tag: "datePicker")
+        datePicker.show(supportFragmentManager,  "datePicker")
     }
 
     fun onDateSelected(day: Int, month: Int, year: Int){
+        etDate!!.setText("has seleccionado el dia $day del mes $month de $year ")
 
     }
 
